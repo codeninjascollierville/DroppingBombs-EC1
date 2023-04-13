ed2c687c20d24b848dd048251f4011767296d5db
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-04-13 18:17:01 -0500

    bux

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,20 +6,57 @@ public class GameManager : MonoBehaviour
 {
     private Spawner spawner;
     public GameObject title;
+    private Vector2 screenBounds;
+    public GameObject playerPrefab;
+    private GameObject player;
+    private bool gameStarted = false;
+    public GameObject splash;
 
     void Awake() {
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        player = playerPrefab;
     }
 
     void Start() {
         spawner.active = false;
+        title.SetActive(true);
+        splash.SetActive(false);
     }
 
     void Update()
     {
-        if (Input.anyKeyDown) {
-            spawner.active = true;
-            title.SetActive(false);
+        if (!gameStarted) {
+            if (Input.anyKeyDown) {
+                ResetGame();
+            }
+        } else {
+            if (!player) {
+                OnPlayerKilled();
+            }
         }
+
+        var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+
+        foreach (GameObject bombObject in nextBomb) {
+            if (bombObject.transform.position.y < (-screenBounds.y) - 12 || !gameStarted) {
+                Destroy(bombObject);
+            }
+        }
+    }
+
+    void ResetGame() {
+        spawner.active = true;
+        title.SetActive(false);
+        splash.SetActive(false);
+        player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
+        gameStarted = true;
+    }
+
+    void OnPlayerKilled() {
+        spawner.active = false;
+        gameStarted = false;
+
+        splash.SetActive(true);
     }
 }
